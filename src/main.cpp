cab6ef6cc5fe4234c35935fa58ade5f619300b6c
huluhuluu <hnuzjh@hnu.edu.cn>
2024-03-11 07:37:21 -0700

    modify graph partition

--- src/main.cpp
+++ src/main.cpp
@@ -6,86 +6,30 @@ using namespace std;
 #include "include/robot.h"
 #include "include/ship.h"
 
-
-void init_map() {
-
-    memset(gds, -1, sizeof(gds));
-    memset(dis, -1, sizeof(dis));
-    memset(global_dis, -1, sizeof(global_dis));
-
-    // * 加载地图 (200 x 200 grids)
-    for (int i = 0; i < n; i++) {
-        scanf("%s", ch[i]);
-    }
-    // * 加载泊位 (10 lines, 5 columns)
-    for (int i = 0; i < BERTH_NUM; i++) {
-        int id;
-        scanf("%d", &id);
-        scanf("%d %d %d %d", &berth[id].x, &berth[id].y, &berth[id].transport_time, &berth[id].loading_speed);
-        // ? 将泊位按照装载速度排序
-        order_of_berth.push_back({berth[id].loading_speed, id});
-    }
-    sort(order_of_berth.begin(), order_of_berth.end(), greater<pair<int, int>>());
-    // ! Debug
-    Debug("berth order:\n");
-    for (auto &i: order_of_berth)
-        Debug("berth_id: %d | loading_speed: %d\n", i.second, i.first);
-    // * 轮船容积
-    scanf("%d", &ship_capacity);
-
-    // end of init
-    scanf("%s", okk);
-    printf("OK\n");
-    fflush(stdout);
-}
-
 bool que_finish[BERTH_NUM];
 
-// TODO: 子图划分，采用 BFS
-void graph_partition() {
-    Debug("[INIT] graph partition\n");
-    // 从每个 BERTH 开始 BFS，遇到障碍或海洋则停止
-    vector<queue<int>> ques(BERTH_NUM);
-    for (int i = 0; i < BERTH_NUM; i++) {
-        gds[berth[i].x][berth[i].y] = i;
-        ques[i].push(berth[i].x * n + berth[i].y);
-    }
-
-    int finish_count = 0;
-    while (finish_count < BERTH_NUM) {
-        for (int i = 0; i < BERTH_NUM; i++) {
-            if (ques[i].empty()) {
-                if (!que_finish[i]) {
-                    que_finish[i] = true;
-                    finish_count++;
-                }
-                continue;
-            }
-            int cur = ques[i].front();
-            ques[i].pop();
-            int x = cur / n, y = cur % n;
-            for (int j = 0; j < 4; j++) {
-                int nxt_x = x + magic_directions[j], nxt_y = y + magic_directions[j + 1];
-                if (nxt_x < 0 || nxt_x >= n || nxt_y < 0 || nxt_y >= n) continue;
-                if (ch[nxt_x][nxt_y] == SEA || ch[nxt_x][nxt_y] == BORDER || gds[nxt_x][nxt_y] != -1) continue;
-                // 标记为已访问
-                gds[nxt_x][nxt_y] = i;
-                ques[i].push(nxt_x * n + nxt_y);
-            }
-        }
-    }
-}
-
+// 子图划分，采用 BFS
 void global_graph_partition() {
     Debug("[INIT] global graph partition\n");
     // 从每个 BERTH 开始 BFS，遇到障碍或海洋则停止
     vector<queue<int>> ques(BERTH_NUM);
     // TODO: 考虑用多线程实现
+    
     for (int b = 0; b < BERTH_NUM; b++) {
-
-        global_dis[b][berth[b].x][berth[b].y] = 0;
-        ques[b].push(berth[b].x * n + berth[b].y);
-
+        // global_dis[b][berth[b].x][berth[b].y] = 0;
+        // ques[b].push(berth[b].x * n + berth[b].y);
+        // 矩形泊位
+        for(int dx = 0; dx < BERTH_SIZE; dx++){
+            for(int dy = 0; dy < BERTH_SIZE; dy++){
+                int nxt_x = berth[b].x + dx, nxt_y = berth[b].y + dy;
+                // if (nxt_x < 0 || nxt_x >= n || nxt_y < 0 || nxt_y >= n) continue;
+                // if (ch[nxt_x][nxt_y] == SEA || ch[nxt_x][nxt_y] == BORDER || global_dis[b][nxt_x][nxt_y] != -1) continue;
+                // gds[nxt_x][nxt_y.y] = b; // todo ? goods initial pos in berth
+                global_dis[b][nxt_x][nxt_y] = 0;
+                ques[b].push((nxt_x) * n + (nxt_y));
+            }
+        }
+        
         while (!ques[b].empty()) {
             int cur = ques[b].front();
             ques[b].pop();
@@ -107,20 +51,57 @@ void global_graph_partition() {
     }  // end of berth loop
 
     // 计算每个点到各个泊位的全局最短距离
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++)
-            min_dis[i][j] = dis[i][j];
-    }
     for (int b = 0; b < BERTH_NUM; b++) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (global_dis[b][i][j] != -1 && global_dis[b][i][j] < min_dis[i][j]) {
-                    min_dis[i][j] = global_dis[b][i][j];
+                if (global_dis[b][i][j] != -1 && (global_dis[b][i][j] < dis[i][j] || dis[i][j] == -1)) {
+                    dis[i][j] = global_dis[b][i][j];
                     gds[i][j] = b;  // 修正分区结果
                 }
             }
         }
     }  // end of berth loop
+}
+
+void init_map() {
+
+    memset(gds, -1, sizeof(gds));
+    memset(dis, -1, sizeof(dis));
+    memset(global_dis, -1, sizeof(global_dis));
+
+    // * 加载地图 (200 x 200 grids)
+    for (int i = 0; i < n ; i++) {
+        scanf("%s", ch[i]);
+    }
+    // * 加载泊位 (10 lines, 5 columns)
+    for (int i = 0; i < BERTH_NUM; i++) {
+        int id;
+        scanf("%d", &id);
+        scanf("%d %d %d %d", &berth[id].x, &berth[id].y, &berth[id].transport_time, &berth[id].loading_speed);
+        // ? 将泊位按照装载速度排序
+        order_of_berth.push_back({berth[id].loading_speed, id});
+    }
+    sort(order_of_berth.begin(), order_of_berth.end(), greater<pair<int, int>>());
+    // ! Debug
+    // Debug("berth order:\n");
+    // for (auto &i: order_of_berth)
+    //     Debug("berth_id: %d | loading_speed: %d\n", i.second, i.first);
+
+    // * 轮船容积
+    scanf("%d", &ship_capacity);
+    scanf("%s", okk);
+
+    // init graph
+    global_graph_partition();
+
+    // init robot
+    for(int i = 0; i < ROBOT_NUM; i++){
+        robot[i].status = rns::ROBOT_WORKING;
+        robot[i].target = rns::T_NONE;
+    }
+    // end of init
+    printf("OK\n");
+    fflush(stdout);
 }
 
 // ! Debug
@@ -188,7 +169,11 @@ int process_inputs(int frame) {
     // TODO: ...
     Debug("%05d F | process_inputs\n", frame);
 
+    // process robot
     execute_robot_instructions(frame);
+
+    // process ship
+    check_ship();
 
     return 0;
 }
@@ -200,14 +185,11 @@ int main() {
 
     init_map();
 
-    graph_partition();
-    // ! 暂时禁用，耗时太长
-    // global_graph_partition();
-
-    show_gds();
-    // ! 暂时禁用，耗时太长
+    // get information of maps
+    // show_gds();
     // show_global_dis();
-
+    // show_dis();
+    
     for (int frame = 1; frame <= FRAME_NUM; frame++) {
         // start of frame
         int id = get_inputs(frame);
